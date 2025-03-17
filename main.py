7ad661ee7f991e079f0d95e0d214540fcc1213b9
Xavier Tepeli <164570151+verybrickednexus5x@users.noreply.github.com>
2025-03-18 09:01:39 +1100

    Create main.py
    created a non-annotated main.py file.

---
+++ main.py
@@ -0,0 +1,58 @@
+def on_button_pressed_a():
+    ship.move(-1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def End_Sound():
+    for index in range(8):
+        music.play(music.string_playable("C5 B A G F E D C ", 1200),
+            music.PlaybackMode.UNTIL_DONE)
+
+def on_button_pressed_ab():
+    global shoot
+    shoot = game.create_sprite(ship.get(LedSpriteProperty.X), ship.get(LedSpriteProperty.Y))
+    shoot.change(LedSpriteProperty.BRIGHTNESS, 80)
+    for index2 in range(4):
+        shoot.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+        if shoot.is_touching(enemy):
+            game.add_score(1)
+            enemy.delete()
+            shoot.delete()
+            music.play(music.tone_playable(330, music.beat(BeatFraction.QUARTER)),
+                music.PlaybackMode.UNTIL_DONE)
+    shoot.delete()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    ship.move(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+enemy: game.LedSprite = None
+shoot: game.LedSprite = None
+ship: game.LedSprite = None
+ship = game.create_sprite(2, 4)
+game.set_score(0)
+
+def on_forever():
+    global enemy
+    enemy = game.create_sprite(randint(0, 4), 0)
+    enemy.change(LedSpriteProperty.DIRECTION, 1)
+    basic.pause(100)
+    enemy.turn(Direction.RIGHT, 90)
+    for index3 in range(4):
+        enemy.move(1)
+        basic.pause(500)
+    if enemy.is_touching_edge():
+        enemy.delete()
+basic.forever(on_forever)
+
+def on_forever2():
+    if enemy.is_touching(ship):
+        game.game_over()
+basic.forever(on_forever2)
+
+def on_forever3():
+    if enemy.is_touching(ship):
+        End_Sound()
+        music.stop_all_sounds()
+basic.forever(on_forever3)
